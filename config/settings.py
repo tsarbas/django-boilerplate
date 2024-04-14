f56427c46c22baabb95b7c88762ef696f594e3b7
timur.sarbassov <timur.sarbassov@welltech.com>
2024-04-14 16:40:16 +0500

    Update STATICFILES_DIRS path in settings.py and build:css and dev:css scripts in package.json

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [BASE_DIR / "staticfiles" / "dist"]
+STATICFILES_DIRS = [BASE_DIR / "staticfiles" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
